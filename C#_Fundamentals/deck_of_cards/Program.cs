34ed244f31c148ae69d3a443bbe145fa20e23539
Catrinici <catrinici.antonina@gmail.com>
2017-04-04 22:35:46 -0700

    deck of cards

---
+++ C#_Fundamentals/deck_of_cards/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BlackJack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play some BlackJack!");
+            Deck MyDeck = new Deck();
+            MyDeck.Shuffle();
+            Player Nelu = new Player("Nelu");
+            
+            Nelu.Draw(MyDeck);
+            Nelu.Draw(MyDeck);
+            Nelu.Draw(MyDeck);
+            Nelu.Draw(MyDeck);
+            Nelu.Draw(MyDeck);
+            Console.WriteLine(Nelu.Hand);
+            
+            Nelu.Discard(3);
+
+            System.Console.WriteLine(Nelu.Hand);
+            
+
+            
+            
+        }
+    }
+}
